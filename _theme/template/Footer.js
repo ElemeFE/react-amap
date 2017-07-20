40bd919030163e6049606dae5902386a494f6d0e
ioslh <ohislh@gmail.com>
2017-07-20 13:10:07 +0800

    add a md doc fix style

--- _theme/template/Footer.js
+++ _theme/template/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
   return <footer>
     <div className="footer-inner">
-      with ❤ by Eleme
+      with ❤ by <a href="https://github.com/ElemeFE/" target="_blank">ElemeFE</a>
     </div>
   </footer>;
 }
